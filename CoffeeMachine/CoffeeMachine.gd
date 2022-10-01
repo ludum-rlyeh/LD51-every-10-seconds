91399684302bf44368ecc26c86b00b0ac77c7141
riffet jérémy <jeremy.riffet@gmail.com>
2022-10-01 23:40:12 +0200

    update the preparation step

--- CoffeeMachine/CoffeeMachine.gd
+++ CoffeeMachine/CoffeeMachine.gd
@@ -2,6 +2,7 @@ extends Control
 
 var _initial_position_coffee_container
 var _initial_position_teapot
+var _initial_position_cup
 
 var _coffee_container_held = false
 var _teapot_held = false
@@ -9,17 +10,21 @@ var _in_milk_area = false
 var _in_coffee_machine_milk_area = false
 var _in_coffee_smasher_area = false
 var _in_coffee_machine_coffee_area = false
-var _is_coffee_container_locked = false
+var _in_coffee_machine_cup_area = false
+var _cup_held = false
 
 func _ready():
 	_initial_position_coffee_container = $CoffeeContainer.rect_global_position
 	_initial_position_teapot = $Teapot.rect_global_position
+	_initial_position_cup = $CupOpened.rect_global_position
 
 func _process(delta: float) -> void:
 	if _coffee_container_held:
 		$CoffeeContainer.rect_global_position = self.get_global_mouse_position()
 	elif _teapot_held:
 		$Teapot.rect_global_position = self.get_global_mouse_position()
+	elif _cup_held:
+		$CupOpened.rect_global_position = self.get_global_mouse_position()
 		
 ###### Teapot
 
@@ -63,7 +68,7 @@ func _on_Milk_pressed() -> void:
 ##### Coffee container
 		
 func _on_CoffeeContainer_button_up() -> void:
-	if _is_coffee_container_locked:
+	if $CoffeeContainer.locked:
 		return
 	_coffee_container_held = false
 	if _in_coffee_smasher_area:
@@ -76,7 +81,7 @@ func _on_CoffeeContainer_button_up() -> void:
 		$CoffeeContainer.rect_global_position = _initial_position_coffee_container
 
 func _on_CoffeeContainer_button_down() -> void:
-	if _is_coffee_container_locked:
+	if $CoffeeContainer.locked:
 		return
 	_coffee_container_held = true
 
@@ -119,3 +124,25 @@ func _on_MilkController_pressed() -> void:
 func _active_milk_controller():
 	$MilkController.flip_h = not $MilkController.flip_h
 	$MilkController.rect_rotation = 160 if $MilkController.rect_rotation == 0 else 0
+
+func _on_CupOpened_area_entered(area: Area2D) -> void:
+	if area.name == "CoffeeMachineCupArea2D":
+		_in_coffee_machine_cup_area = true
+	
+func _on_CupOpened_area_exited(area: Area2D) -> void:
+	if area.name == "CoffeeMachineCupArea2D":
+		_in_coffee_machine_cup_area = false
+
+func _on_CupOpened_button_down() -> void:
+	if $CupOpened.locked:
+		return
+	_cup_held = true
+
+func _on_CupOpened_button_up() -> void:
+	if $CupOpened.locked:
+		return
+	_cup_held = false
+	if _in_coffee_machine_cup_area:
+		$CupOpened.rect_global_position = $Machine/Coffee/CupPosition2D.global_position
+	else:
+		$CupOpened.rect_global_position = _initial_position_cup
