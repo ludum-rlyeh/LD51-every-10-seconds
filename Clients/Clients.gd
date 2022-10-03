0f9d38e822601a728b036e561eb21cb2ea76f455
Avzgui <contact.avzgui@gmail.com>
2022-10-03 19:04:51 +0200

    balance music and sfx

--- Clients/Clients.gd
+++ Clients/Clients.gd
@@ -2,10 +2,14 @@ extends HBoxContainer
 
 signal client_selected
 
+var current_client = null
+
 func add_client(client):
+	current_client = client
 	add_child(client)
 	
 func remove_client(client):
+	current_client = null
 	remove_child(client)
 
 func _on_client_pressed(client):
@@ -14,3 +18,11 @@ func _on_client_pressed(client):
 func _on_new_client_waiting(client) -> void:
 	add_client(client)
 	client.connect("pressed", self, "_on_client_pressed", [client])
+
+func deactivate_client():
+	if current_client:
+		current_client.disabled = true
+
+func activate_client():
+	if current_client:
+		current_client.disabled = false
